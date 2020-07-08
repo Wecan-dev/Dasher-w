38c63e55e2994b2242b6926716fcce52be9d217d
dannyunico <danielbermudezreyes@gmail.com>
2020-07-08 15:24:39 -0400

    add view category

--- assets/js/setting-slick.js
+++ assets/js/setting-slick.js
@@ -75,6 +75,50 @@ $('.main-cards__slider').slick({
   ]
 });
 
+$('.main-cards--category__slider').slick({
+  infinite: true,
+  autoplay: false,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  dots: true,
+  arrows: false,
+  centerMode:true,
+  centerPadding:'40px',
+  responsive: [{
+    breakpoint: 1200,
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      infinite: true,
+      dots: false
+    }
+  },
+  {
+    breakpoint: 900,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 2,
+      arrows: false,
+    }
+  },
+  {
+    breakpoint: 600,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots: false,
+      autoplay: false,
+      autoplaySpeed: 1000,
+      arrows:false,
+      centerMode: true,
+      centerPadding: '25px',
+    }
+  }
+  ]
+});
+
+
+
 $('.main-nosotros__content').slick({
   infinite: true,
   autoplay: false,
