3cc9cf77177c9ec6e65dcbac2e3694e122080fbd
misledy <misledy1@hotmail.com>
2020-07-28 13:28:47 -0400

    Customizer home banner info

--- wp-content/themes/Dasher/functions.php
+++ wp-content/themes/Dasher/functions.php
@@ -39,78 +39,23 @@ function dasher_styles(){
 
 add_action('wp_enqueue_scripts', 'dasher_styles');
 
+/***************Functions theme ********************/
 
+function theme_customize_register($wp_customize){
+  $wp_customize->add_panel('panel1',
+        array(
+            'title' => 'Secciones Home',
+            'priority' => 1,
+            )
+        );
+  require_once trailingslashit( get_template_directory() ) . 'inc/customizer-home-banner.php';
+  
 
-/**truncar cantidad de palabras***/
-function excerpt($limit) {
-	$excerpt = explode(' ', get_the_excerpt(), $limit);
-	if (count($excerpt)>=$limit) {
-		array_pop($excerpt);
-		$excerpt = implode(" ",$excerpt).'...';
-	} else {
-		$excerpt = implode(" ",$excerpt);
-	}
-	$excerpt = preg_replace('`[[^]]*]`','',$excerpt);
-	return $excerpt;
-}
-/**truncar cantidad de palabras***/
+} 
+add_action('customize_register','theme_customize_register');
 
+/***************** FNT General ************/
 
-// Register Custom Banner Home
-function Banner() {
-
-	$labels = array(
-		'name'                  => _x( 'Banner ', 'Post Type General Name', 'fefi' ),
-		'singular_name'         => _x( 'Banner', 'Post Type Singular Name', 'fefi' ),
-		'menu_name'             => __( 'Banners', 'fefi' ),
-		'name_admin_bar'        => __( 'Post Type', 'fefi' ),
-		'archives'              => __( 'Archivo', 'fefi' ),
-		'attributes'            => __( 'Atributos', 'fefi' ),
-		'parent_item_colon'     => __( 'Artículo principal', 'fefi' ),
-		'all_items'             => __( 'Todos los artículos', 'fefi' ),
-		'add_new_item'          => __( 'Agregar ítem nuevo', 'fefi' ),
-		'add_new'               => __( 'Añadir nuevo', 'fefi' ),
-		'new_item'              => __( 'Nuevo artículo', 'fefi' ),
-		'edit_item'             => __( 'Editar elemento', 'fefi' ),
-		'update_item'           => __( 'Actualizar artículo', 'fefi' ),
-		'view_item'             => __( 'Ver ítem', 'fefi' ),
-		'view_items'            => __( 'Ver artículos', 'fefi' ),
-		'search_items'          => __( 'Buscar artículo', 'fefi' ),
-		'not_found'             => __( 'Extraviado', 'fefi' ),
-		'not_found_in_trash'    => __( 'No se encuentra en la basura', 'fefi' ),
-		'featured_image'        => __( 'Foto principal', 'fefi' ),
-		'set_featured_image'    => __( 'Establecer imagen destacada', 'fefi' ),
-		'remove_featured_image' => __( 'Remove featured image', 'fefi' ),
-		'use_featured_image'    => __( 'Usar como imagen destacada', 'fefi' ),
-		'insert_into_item'      => __( 'Insertar en el elemento', 'fefi' ),
-		'uploaded_to_this_item' => __( 'Subido a este artículo', 'fefi' ),
-		'items_list'            => __( 'Lista de artículos', 'fefi' ),
-		'items_list_navigation' => __( 'Lista de elementos de navegación', 'fefi' ),
-		'filter_items_list'     => __( 'Lista de elementos de filtro', 'fefi' ),
-	);
-	$args = array(
-		'label'                 => __( 'Banner Home', 'fefi' ),
-		'description'           => __( 'Post Type Description', 'fefi' ),
-		'labels'                => $labels,
-		'supports'              => array( 'title', 'editor', 'thumbnail' ),
-		'taxonomies'            => array(  ),
-		'hierarchical'          => false,
-		'public'                => true,
-		'show_ui'               => true,
-		'show_in_menu'          => true,
-		'menu_position'         => 5,
-		'menu_icon'             => 'dashicons-images-alt2',
-		'show_in_admin_bar'     => true,
-		'show_in_nav_menus'     => true,
-		'can_export'            => true,
-		'has_archive'           => true,
-		'exclude_from_search'   => false,
-		'publicly_queryable'    => true,
-		'capability_type'       => 'page',
-	);
-	register_post_type( 'Banner', $args );
-
-}
-add_action( 'init', 'Banner', 0 );
+require_once trailingslashit( get_template_directory() ) . 'inc/fnt/fnt.php';
 
 
---
+++ wp-content/themes/Dasher/partials/home/banner.php
@@ -0,0 +1,106 @@
+<section class="pb-5">
+	<div class="banner">
+		<div class="main-banner">
+			<div class="main-banner__content">
+				<div class="main-banner__item">
+					<div class="main-banner__text">
+						<div class="main-banner__title title-general">
+							<p>Lorem ipsum dolor sit amet</p>
+						</div>
+						<div class="subtitle-general">
+							<p>El mejor delivery de tu ciudad</p>
+						</div>
+						<div class="boton-banner">
+							<a class="btn btn-banner boton-general" href="#">Llama ya</a>
+						</div>
+					</div>
+					<div class="main-banner__img">
+						<div class="main-banner__img--content">
+							<img class="d-none d-md-block" src="<?php echo get_template_directory_uri();?>/assets/img/banner home.png" alt="">
+							<img class="d-block d-md-none" src="<?php echo get_template_directory_uri();?>/assets/img/banner respon.png" alt="">
+						</div>
+					</div>
+				</div>
+				<div class="main-banner__item">
+					<div class="main-banner__text">
+						<div class="main-banner__title title-general">
+							<p>xxxxxx</p>
+						</div>
+						<div class="subtitle-general">
+							<p>El mejor delivery de tu ciudad</p>
+						</div>
+						<div class="boton-banner">
+							<a class="btn btn-banner boton-general" href="#">Llama ya</a>
+						</div>
+					</div>
+					<div class="main-banner__img">
+						<div class="main-banner__img--content">
+							<img class="d-none d-md-block" src="<?php echo get_template_directory_uri();?>/assets/img/banner home.png" alt="">
+							<img class="d-block d-md-none" src="<?php echo get_template_directory_uri();?>/assets/img/banner respon.png" alt="">
+						</div>
+					</div>
+				</div>
+
+				<div class="main-banner__item">
+					<div class="main-banner__text">
+						<div class="main-banner__title title-general">
+							<p>hhhhhh</p>
+						</div>
+						<div class="subtitle-general">
+							<p>El mejor delivery de tu ciudad</p>
+						</div>
+						<div class="boton-banner">
+							<a class="btn btn-banner boton-general" href="#">Llama ya</a>
+						</div>
+					</div>
+					<div class="main-banner__img">
+						<div class="main-banner__img--content">
+							<img class="d-none d-md-block" src="<?php echo get_template_directory_uri();?>/assets/img/banner home.png" alt="">
+							<img class="d-block d-md-none" src="<?php echo get_template_directory_uri();?>/assets/img/banner respon.png" alt="">
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+	<div class="subanner padding-rl fadeInUp wow"  >
+		<div class="subanner-content">
+			<a href="<?php the_permalink(); ?>"> 
+				<img src="<?php echo get_template_directory_uri();?>/assets/img/restaurantes.png" alt="">
+				<p>Resturantes</p>
+			</a>
+		</div>
+		<div class="subanner-content">
+			<a href="#">
+				<img src="<?php echo get_template_directory_uri();?>/assets/img/bodegones.png" alt="">
+				<p>Bodegón</p>
+			</a>
+		</div>
+		<div class="subanner-content">
+			<a href="#">
+				<img src="<?php echo get_template_directory_uri();?>/assets/img/farmacias.png" alt="">
+				<p>Viveres</p>
+			</a>
+		</div>
+		<div class="subanner-content">
+			<a href="#">
+				<img src="<?php echo get_template_directory_uri();?>/assets/img/viveres.png" alt="">
+				<p>Farmacias</p>
+			</a>
+		</div>
+		<div class="subanner-content">
+			<a href="#">
+				<img src="<?php echo get_template_directory_uri();?>/assets/img/shopping.png" alt="">
+				<p>Shopping</p>
+			</a>
+		</div>
+	</div>
+	<div class="search-content">
+		
+		<button class="" type="submit"><img src="<?php echo get_template_directory_uri();?>/assets/img/serch bottom.png" alt=""></button>
+	    <input class="form-control mr-sm-2" type="search" placeholder="Busca tus productos o tiendas favoritas" aria-label="Search">
+	    <button class="btn btn-outline btn-search" type="submit">Buscar</button>
+	    
+    </div>
+</section>
