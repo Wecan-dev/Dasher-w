f0b3d98525d7779c0a46bfb93d5a8dd7dda0b5b0
misledy <misledy1@hotmail.com>
2020-07-29 13:55:47 -0400

    Cards-product, filter product by lo más nuevo

--- wp-content/themes/Dasher/partials/home/cards-products.php
+++ wp-content/themes/Dasher/partials/home/cards-products.php
@@ -33,7 +33,7 @@
 				array(
 					'post_type' => 'product',
 					'paged' => $paged,
-					'posts_per_page' => 9,        
+					'posts_per_page' => 100,        
 					'post_status' => 'publish',
                     'orderby' => 'meta_value', // orderby the meta_value of the following meta_key
                     'meta_key' => 'total_sales', // the custom meta_key name
@@ -76,14 +76,21 @@
 				<p>Los más nuevos</p>
 			</div>
 			<div class="main-cards__slider padding-rl">
+            <?php
+            $args = array (
+               'post_type' => 'product',
+               'posts_per_page' => 100,
+            );
+            $loop = new WP_Query( $args ); 
+            while ( $loop->have_posts() ) : $loop->the_post(); global $product; ?>    				
 				<div class="main-cards__slider--content">
-					<a href="">
+					<a href="<?php the_permalink(); ?>">
 						<div class="slider-card__img">
-							<img src="<?php echo get_template_directory_uri();?>/assets/img/card-product.jpeg" alt="">
+							<img src="<?php the_post_thumbnail_url('full'); ?>" alt="">
 						</div>
 						<div class="slider-card__content">
 							<div class="slider-card__content--title">
-								<p>Nombre del producto</p>
+								<p><?php the_title(); ?></p>
 								<small>valoración</small>
 								<span>3 pts</span>
 							</div>
@@ -92,7 +99,7 @@
 								<p>Abierto</p>
 							</div>
 							<div class="slider-card__content--text">
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem velit magni magnam eius officia quaerat quae reiciendis aut ratione sit.</p>
+								<p><?php  echo strip_tags(cut_text(content(get_the_ID(),'product'),20));?></p>
 								<div class="content--text__logo">
 									<img src="" alt="">
 								</div>
@@ -100,78 +107,7 @@
 						</div>
 					</a>
 				</div>
-				<div class="main-cards__slider--content">
-					<a href="">
-						<div class="slider-card__img">
-							<img src="<?php echo get_template_directory_uri();?>/assets/img/card-product.jpeg" alt="">
-						</div>
-						<div class="slider-card__content">
-							<div class="slider-card__content--title">
-								<p>Nombre del producto</p>
-								<small>valoración</small>
-								<span>3 pts</span>
-							</div>
-							<div class="slider-card__content--etiqueta">
-								<div class="content--etiqueta__color"></div>
-								<p>Abierto</p>
-							</div>
-							<div class="slider-card__content--text">
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem velit magni magnam eius officia quaerat quae reiciendis aut ratione sit.</p>
-								<div class="content--text__logo">
-									<img src="" alt="">
-								</div>
-							</div>
-						</div>
-					</a>
-				</div>
-				<div class="main-cards__slider--content">
-					<a href="">
-						<div class="slider-card__img">
-							<img src="<?php echo get_template_directory_uri();?>/assets/img/card-product.jpeg" alt="">
-						</div>
-						<div class="slider-card__content">
-							<div class="slider-card__content--title">
-								<p>Nombre del producto</p>
-								<small>valoración</small>
-								<span>3 pts</span>
-							</div>
-							<div class="slider-card__content--etiqueta">
-								<div class="content--etiqueta__color"></div>
-								<p>Abierto</p>
-							</div>
-							<div class="slider-card__content--text">
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem velit magni magnam eius officia quaerat quae reiciendis aut ratione sit.</p>
-								<div class="content--text__logo">
-									<img src="" alt="">
-								</div>
-							</div>
-						</div>
-					</a>
-				</div>
-				<div class="main-cards__slider--content">
-					<a href="">
-						<div class="slider-card__img">
-							<img src="<?php echo get_template_directory_uri();?>/assets/img/card-product.jpeg" alt="">
-						</div>
-						<div class="slider-card__content">
-							<div class="slider-card__content--title">
-								<p>Nombre del producto</p>
-								<small>valoración</small>
-								<span>3 pts</span>
-							</div>
-							<div class="slider-card__content--etiqueta">
-								<div class="content--etiqueta__color"></div>
-								<p>Abierto</p>
-							</div>
-							<div class="slider-card__content--text">
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem velit magni magnam eius officia quaerat quae reiciendis aut ratione sit.</p>
-								<div class="content--text__logo">
-									<img src="" alt="">
-								</div>
-							</div>
-						</div>
-					</a>
-				</div>
+			<?php endwhile; ?>	
 			</div>
 		</div>
 	</div>
