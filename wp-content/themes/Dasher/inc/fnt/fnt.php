56feef84e56ae5af66a38d8c2a50df352a17ef4a
misledy <misledy1@hotmail.com>
2020-07-29 13:42:33 -0400

    Cards-product, filter products popularity

--- wp-content/themes/Dasher/inc/fnt/fnt.php
+++ wp-content/themes/Dasher/inc/fnt/fnt.php
@@ -11,7 +11,62 @@ add_theme_support( 'post-thumbnails' );
 the_post_thumbnail( array(100,100) ); 
 set_post_thumbnail_size( 1568, 9999 );
 
+// Add default posts and comments RSS feed links to head.
+add_theme_support( 'automatic-feed-links' );
+
+/*
+ * Let WordPress manage the document title.
+ * By adding theme support, we declare that this theme does not use a
+ * hard-coded <title> tag in the document head, and expect WordPress to
+ * provide it for us.
+ *
+ */add_theme_support( 'title-tag' );
+
+/*
+ * Enable support for Post Thumbnails on posts and pages.
+*
+* @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
+ */
+add_theme_support( 'post-thumbnails' );
+
+    // This theme uses wp_nav_menu() in one location.
+register_nav_menus( array(
+	'primary' => __( 'Primary Menu', 'store' ),
+	'top' => __( 'Top Menu', 'store' ),
+) );
+
+/*
+ * Switch default core markup for search form, comment form, and comments
+ * to output valid HTML5.
+ */
+add_theme_support( 'html5', array(
+	'search-form', 'comment-form', 'comment-list', 'gallery', 'caption',
+) );
+
+/*
+ * Enable support for Post Formats.
+ * See http://codex.wordpress.org/Post_Formats
+ */
+add_theme_support( 'post-formats', array(
+    'aside', 'image', 'video', 'quote', 'link',
+) );
+
+// Set up the WordPress core custom background feature.
+add_theme_support( 'custom-background', apply_filters( 'store_custom_background_args', array(
+    'default-color' => 'f7f5ee',
+    'default-image' => '',
+) ) );
+
+add_image_size('store-sq-thumb', 600,600, true );
+add_image_size('store-thumb', 540,450, true );
+add_image_size('pop-thumb',542, 340, true );
+
+//Declare woocommerce support
+add_theme_support('woocommerce');
+add_theme_support( 'wc-product-gallery-lightbox' );
+
 /**Excerpt general***/
+
 function excerpt($limit) {
 	$excerpt = explode(' ', get_the_excerpt(), $limit);
 	if (count($excerpt)>=$limit) {
@@ -24,5 +79,28 @@ function excerpt($limit) {
 	return $excerpt;
 }
 
+/******************Excerp*****************/
+function cut_text($text, $le) 
+{ 
+   $points = "...";
+   $words = explode(' ', $text); 
+   if (count($words) > $le) 
+   { 
+     return implode(' ', array_slice($words, 0, $le)) ." ". $points; 
+   } else
+         {
+           return $text; 
+         } 
+}
+
+/************* Content *******************************/
+function content($id,$type){
+    global $wpdb;
+    $result = $wpdb->get_results ("SELECT * FROM ".$wpdb->prefix."posts where ID = '$id' and post_type = '$type'");
+    foreach ( $result as $page )
+    {   
+      return $page->post_content;
+    }  
+}
 
 ?>
--- wp-content/themes/Dasher/partials/home/cards-products.php
+++ wp-content/themes/Dasher/partials/home/cards-products.php
@@ -29,14 +29,27 @@
 				<p>Los más Populares</p>
 			</div>
 			<div class="main-cards__slider padding-rl">
+			<?php $args = 
+				array(
+					'post_type' => 'product',
+					'paged' => $paged,
+					'posts_per_page' => 9,        
+					'post_status' => 'publish',
+                    'orderby' => 'meta_value', // orderby the meta_value of the following meta_key
+                    'meta_key' => 'total_sales', // the custom meta_key name
+                    'order'=> 'DESC' // sort descending
+                );
+            ?>
+            <?php $loop = new WP_Query( $args ); ?>
+            <?php while ( $loop->have_posts() ) : $loop->the_post(); global $product;?>				
 				<div class="main-cards__slider--content">
-					<a href="">
+					<a href="<?php the_permalink(); ?>">
 						<div class="slider-card__img">
-							<img src="<?php echo get_template_directory_uri();?>/assets/img/card-product.jpeg" alt="">
+							<img src="<?php the_post_thumbnail_url('full'); ?>" alt="">
 						</div>
 						<div class="slider-card__content">
 							<div class="slider-card__content--title">
-								<p>Nombre del producto</p>
+								<p><?php the_title(); ?></p>
 								<small>valoración</small>
 								<span>3 pts</span>
 							</div>
@@ -45,7 +58,7 @@
 								<p>Abierto</p>
 							</div>
 							<div class="slider-card__content--text">
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem velit magni magnam eius officia quaerat quae reiciendis aut ratione sit.</p>
+								<p><?php  echo strip_tags(cut_text(content(get_the_ID(),'product'),20));?></p>
 								<div class="content--text__logo">
 									<img src="" alt="">
 								</div>
@@ -53,78 +66,7 @@
 						</div>
 					</a>
 				</div>
-				<div class="main-cards__slider--content">
-					<a href="">
-						<div class="slider-card__img">
-							<img src="<?php echo get_template_directory_uri();?>/assets/img/card-product.jpeg" alt="">
-						</div>
-						<div class="slider-card__content">
-							<div class="slider-card__content--title">
-								<p>Nombre del producto</p>
-								<small>valoración</small>
-								<span>3 pts</span>
-							</div>
-							<div class="slider-card__content--etiqueta">
-								<div class="content--etiqueta__color"></div>
-								<p>Abierto</p>
-							</div>
-							<div class="slider-card__content--text">
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem velit magni magnam eius officia quaerat quae reiciendis aut ratione sit.</p>
-								<div class="content--text__logo">
-									<img src="" alt="">
-								</div>
-							</div>
-						</div>
-					</a>
-				</div>
-				<div class="main-cards__slider--content">
-					<a href="">
-						<div class="slider-card__img">
-							<img src="<?php echo get_template_directory_uri();?>/assets/img/card-product.jpeg" alt="">
-						</div>
-						<div class="slider-card__content">
-							<div class="slider-card__content--title">
-								<p>Nombre del producto</p>
-								<small>valoración</small>
-								<span>3 pts</span>
-							</div>
-							<div class="slider-card__content--etiqueta">
-								<div class="content--etiqueta__color"></div>
-								<p>Abierto</p>
-							</div>
-							<div class="slider-card__content--text">
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem velit magni magnam eius officia quaerat quae reiciendis aut ratione sit.</p>
-								<div class="content--text__logo">
-									<img src="" alt="">
-								</div>
-							</div>
-						</div>
-					</a>
-				</div>
-				<div class="main-cards__slider--content">
-					<a href="">
-						<div class="slider-card__img">
-							<img src="<?php echo get_template_directory_uri();?>/assets/img/card-product.jpeg" alt="">
-						</div>
-						<div class="slider-card__content">
-							<div class="slider-card__content--title">
-								<p>Nombre del producto</p>
-								<small>valoración</small>
-								<span>3 pts</span>
-							</div>
-							<div class="slider-card__content--etiqueta">
-								<div class="content--etiqueta__color"></div>
-								<p>Abierto</p>
-							</div>
-							<div class="slider-card__content--text">
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem velit magni magnam eius officia quaerat quae reiciendis aut ratione sit.</p>
-								<div class="content--text__logo">
-									<img src="" alt="">
-								</div>
-							</div>
-						</div>
-					</a>
-				</div>
+		    <?php endwhile; ?> 		
 			</div>
 		</div>
 	</div>
