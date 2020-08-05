222e4506808f8a4eded2bdbc6044c1051395d6bf
misledy <misledy1@hotmail.com>
2020-08-05 13:44:33 -0400

    Add store.php/dokan

--- wp-content/themes/Dasher/inc/fnt/fnt.php
+++ wp-content/themes/Dasher/inc/fnt/fnt.php
@@ -65,6 +65,24 @@ add_image_size('pop-thumb',542, 340, true );
 add_theme_support('woocommerce');
 add_theme_support( 'wc-product-gallery-lightbox' );
 
+/*****************Widge ************************/
+function dosher_widgets_init() {
+
+  register_sidebar(
+    array(
+      'name'          => __( 'Footer', 'Dosher' ),
+      'id'            => 'sidebar-1',
+      'description'   => __( 'Add widgets here to appear in your footer.', 'Dosher' ),
+      'before_widget' => '<section id="%1$s" class="widget %2$s">',
+      'after_widget'  => '</section>',
+      'before_title'  => '<h2 class="widget-title">',
+      'after_title'   => '</h2>',
+    )
+  );
+
+}
+add_action( 'widgets_init', 'dosher_widgets_init' );
+
 /****************Excerpt general****************/
 
 function excerpt($limit) {
